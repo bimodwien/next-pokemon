04cfd30cb5a9b3b76d16f5c5a97b29f6b5a3200f
bimodwien <bimodwien2@gmail.com>
2024-12-03 10:10:04 +0700

    update: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Welcome to Pokemon Web</h1>
+        <p className="text-lg">This is Pokemon website with PokeAPI</p>
+      </main>
+    </div>
+  );
+}
